504856b4e8f7124fc9a05579cc570351df06726f
devson <sonyc5720@gmail.com>
2019-03-06 23:31:16 +0900

    CsvManager test 코드 추가

---
+++ tictactoe/src/test/java/tictactoe/csv/CsvManagerTest.java
@@ -0,0 +1,28 @@
+package tictactoe.csv;
+
+import org.junit.Test;
+import tictactoe.Player;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CsvManagerTest {
+    @Test
+    public void csvManager_savePlayer_readPlayer_test() {
+        Player player = new Player("Chris");
+        player.win();
+        player.win();
+        player.lose();
+        player.lose();
+
+        CsvManager csvManager = new CsvManager();
+
+        Player savedPlayer = csvManager.savePlayer(player);
+        Player readPlayer = csvManager.readPlayer(player);
+
+        assertThat(player, is(savedPlayer));
+        assertThat(player, is(readPlayer));
+        assertThat(savedPlayer, is(readPlayer));
+    }
+
+}
