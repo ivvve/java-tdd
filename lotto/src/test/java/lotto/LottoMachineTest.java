6d77d0a89622342d40a447967f306955a062e8d7
devson <sonyc5720@gmail.com>
2019-03-08 18:18:03 +0900

    bonusNumber (로또에서 보너스)에 대한 테스트 코드 추가

--- lotto/src/test/java/lotto/LottoMachineTest.java
+++ lotto/src/test/java/lotto/LottoMachineTest.java
@@ -9,6 +9,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoMachineTest {
 
     @Test
+    public void winNumbers_가_1과_45_사이의_값인지_테스트() {
+        LottoMachine lottoMachine = new LottoMachine();
+
+        for (int i = 0; i < 1000; i++) {
+            List<Integer> winNumbers = lottoMachine.winNumbers();
+
+            winNumbers.forEach(winNumber -> {
+                assertThat(winNumber)
+                        .isGreaterThanOrEqualTo(1)
+                        .isLessThanOrEqualTo(45);
+            });
+        }
+    }
+
+    @Test
     public void winNumbers_모두_unique한_값인지_테스트() {
         LottoMachine lottoMachine = new LottoMachine();
         List<Integer> winNumbers = lottoMachine.winNumbers();
@@ -20,6 +35,25 @@ public class LottoMachineTest {
                 .count();
 
         assertThat(countOfUniqueWinNumbers).isEqualTo(winNumbers.size());
+    }
+
+    @Test
+    public void bonusNumber_가_1과_45_사이의_값인지_테스트() {
+        LottoMachine lottoMachine = new LottoMachine();
+        int bonusNumber = lottoMachine.bonusNumber();
+
+        assertThat(bonusNumber)
+                .isGreaterThanOrEqualTo(1)
+                .isLessThanOrEqualTo(45);
+    }
+
+    @Test
+    public void bonusNumber_가_winNumbers_에_포함되지_않는지_테스트() {
+        LottoMachine lottoMachine = new LottoMachine();
+        List<Integer> winNumbers = lottoMachine.winNumbers();
+        int bonusNumber = lottoMachine.bonusNumber();
+
+        assertThat(winNumbers).doesNotContain(bonusNumber);
     }
 
 }
