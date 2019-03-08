0446e9c74ddb6240ca0fffa659e114adad9d52be
devson <sonyc5720@gmail.com>
2019-03-08 18:35:00 +0900

    winNumbers와 bounsNumber를 겹치지 않게 하려면 같이 return 해야할 것 같아 LottoMachine 객체의 내부로직 변경

--- lotto/src/test/java/lotto/LottoMachineTest.java
+++ lotto/src/test/java/lotto/LottoMachineTest.java
@@ -9,51 +9,54 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoMachineTest {
 
     @Test
-    public void winNumbers_가_1과_45_사이의_값인지_테스트() {
+    public void winNumbers_모두_unique한_값인지_테스트() {
         LottoMachine lottoMachine = new LottoMachine();
 
         for (int i = 0; i < 1000; i++) {
-            List<Integer> winNumbers = lottoMachine.winNumbers();
+            LottoNumber lottoNumber = lottoMachine.lottoNumber();
+            List<Integer> winNumbers = lottoNumber.winNumbers();
 
-            winNumbers.forEach(winNumber -> {
-                assertThat(winNumber)
-                        .isGreaterThanOrEqualTo(1)
-                        .isLessThanOrEqualTo(45);
-            });
+            long countOfUniqueNumberOfWinNumbers = winNumbers.stream()
+                    .distinct()
+                    .count();
+
+            assertThat(countOfUniqueNumberOfWinNumbers).isEqualTo(winNumbers.size());
         }
-    }
-
-    @Test
-    public void winNumbers_모두_unique한_값인지_테스트() {
-        LottoMachine lottoMachine = new LottoMachine();
-        List<Integer> winNumbers = lottoMachine.winNumbers();
-
-        System.out.println("win numbers are : " + winNumbers);
-
-        long countOfUniqueWinNumbers = winNumbers.stream()
-                .distinct()
-                .count();
-
-        assertThat(countOfUniqueWinNumbers).isEqualTo(winNumbers.size());
-    }
-
-    @Test
-    public void bonusNumber_가_1과_45_사이의_값인지_테스트() {
-        LottoMachine lottoMachine = new LottoMachine();
-        int bonusNumber = lottoMachine.bonusNumber();
-
-        assertThat(bonusNumber)
-                .isGreaterThanOrEqualTo(1)
-                .isLessThanOrEqualTo(45);
     }
 
     @Test
     public void bonusNumber_가_winNumbers_에_포함되지_않는지_테스트() {
         LottoMachine lottoMachine = new LottoMachine();
-        List<Integer> winNumbers = lottoMachine.winNumbers();
-        int bonusNumber = lottoMachine.bonusNumber();
 
-        assertThat(winNumbers).doesNotContain(bonusNumber);
+        for (int i = 0; i < 1000; i++) {
+            LottoNumber lottoNumber = lottoMachine.lottoNumber();
+            List<Integer> winNumbers = lottoNumber.winNumbers();
+            int bonusNumber = lottoNumber.bonusNumber();
+
+            assertThat(winNumbers).doesNotContain(bonusNumber);
+        }
     }
 
+
+    @Test
+    public void lottoNumbers의_범위가_1과_45_사이인지_테스트() {
+        LottoMachine lottoMachine = new LottoMachine();
+
+        for (int i = 0; i < 1000; i++) {
+            LottoNumber lottoNumber = lottoMachine.lottoNumber();
+            List<Integer> winNumbers = lottoNumber.winNumbers();
+            winNumbers.stream()
+                    .forEach(n -> {
+                        assertThat(this.isRightLottoNumber(n)).isEqualTo(true);
+                    });
+
+            int bonusNumber = lottoNumber.bonusNumber();
+            assertThat(this.isRightLottoNumber(bonusNumber)).isEqualTo(true);
+        }
+    }
+
+    private boolean isRightLottoNumber(int number) {
+        return 1 <= number &&
+                number <= 45;
+    }
 }
