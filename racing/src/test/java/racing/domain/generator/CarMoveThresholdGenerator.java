c2c11d20068c9d2cbf544576455f668e39b43c41
devson <sonyc5720@gmail.com>
2019-04-20 08:39:44 +0900

    사용하지 않는 import 제거

--- racing/src/test/java/racing/domain/generator/CarMoveThresholdGenerator.java
+++ racing/src/test/java/racing/domain/generator/CarMoveThresholdGenerator.java
@@ -1,7 +1,5 @@
 package racing.domain.generator;
 
-import racing.domain.generator.NumberGenerator;
-
 public class CarMoveThresholdGenerator implements NumberGenerator {
     private static final int MOVE_THRESHOLD = 4;
 
