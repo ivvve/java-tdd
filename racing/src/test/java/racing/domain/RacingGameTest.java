afb54ef82a9ab63bebce83fecca083522d9633d6
devson <sonyc5720@gmail.com>
2019-04-20 08:41:06 +0900

    RacingGameTest 생성

---
+++ racing/src/test/java/racing/domain/RacingGameTest.java
@@ -0,0 +1,64 @@
+package racing.domain;
+
+import org.junit.Test;
+import racing.domain.generator.CarMoveThresholdGenerator;
+import racing.domain.generator.NumberGenerator;
+import racing.domain.generator.ZeroGenerator;
+import racing.vo.Round;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingGameTest {
+    private NumberGenerator carMoveThresholdGenerator = new CarMoveThresholdGenerator();
+    private NumberGenerator zeroGenerator = new ZeroGenerator();
+
+    @Test
+    public void 라운드만큼_실행() {
+        // given
+        Round totalRound = new Round(5);
+        Cars cars = new Cars(Arrays.asList(moveCar(), moveCar(), moveCar()));
+
+        RacingGame racingGame = new RacingGame(cars, totalRound);
+
+        // when
+        while (!racingGame.hasNextRound()) {
+            racingGame.nextRound();
+        }
+
+        // then
+        assertThat(racingGame.getRound()).isEqualTo(totalRound);
+    }
+
+    @Test
+    public void 승자_가져오기() {
+        // given
+        Round totalRound = new Round(5);
+
+        Car winner1 = moveCar();
+        Car winner2 = moveCar();
+        Car loser = dontMoveCar();
+
+        Cars cars = new Cars(Arrays.asList(winner1, winner2, loser));
+        RacingGame racingGame = new RacingGame(cars, totalRound);
+
+        // when
+        while (!racingGame.hasNextRound()) {
+            racingGame.nextRound();
+        }
+
+        // then
+        List<Car> winners = racingGame.getWinnersOfRound();
+        assertThat(winners).containsExactly(winner1, winner2);
+    }
+
+    private Car moveCar() {
+        return new Car(carMoveThresholdGenerator);
+    }
+
+    private Car dontMoveCar() {
+        return new Car(zeroGenerator);
+    }
+}
