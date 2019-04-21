e370e8d229bfde4fb02235cd05306552a462403a
devson <sonyc5720@gmail.com>
2019-04-21 12:27:56 +0900

    field 값에 1. name 추가 2. distance : int -> Distance로 타입 변경

--- racing/src/main/java/racing/domain/Car.java
+++ racing/src/main/java/racing/domain/Car.java
@@ -1,38 +1,42 @@
 package racing.domain;
 
 import racing.domain.generator.NumberGenerator;
-
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import racing.vo.Distance;
 
 public class Car {
     private static final int MOVE_THRESHOLD = 4;
+    private static final int INITIAL_DISTANCE = 0;
 
+    private final String name;
     private final NumberGenerator numberGenerator;
-    private int distance = 0;
+    private Distance distance;
 
-    public Car(NumberGenerator numberGenerator) {
+    public Car(String name, NumberGenerator numberGenerator) {
+        this.name = name;
         this.numberGenerator = numberGenerator;
+        this.distance = new Distance(INITIAL_DISTANCE);
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " : " + this.distance;
     }
 
     void move() {
         move(numberGenerator.generate());
     }
 
+    Distance getDistance() {
+        return distance;
+    }
+
+    int getDistanceValue() {
+        return this.distance.getValue();
+    }
+
     private void move(int value) {
         if (MOVE_THRESHOLD <= value) {
-            this.distance++;
+            this.distance = this.distance.increment();
         }
-    }
-
-    int getDistance() {
-        return this.distance;
-    }
-
-    @Override
-    public String toString() {
-        return IntStream.range(0, this.distance)
-                .mapToObj(i -> "-")
-                .collect(Collectors.joining());
     }
 }
--- racing/src/test/java/racing/domain/CarTest.java
+++ racing/src/test/java/racing/domain/CarTest.java
@@ -4,48 +4,51 @@ import org.junit.Test;
 import racing.domain.generator.CarMoveThresholdGenerator;
 import racing.domain.generator.NumberGenerator;
 import racing.domain.generator.ZeroGenerator;
+import racing.vo.Distance;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class CarTest {
     @Test
     public void Car_이동_문턱값_이상() {
         // given
+        String name = "son";
         NumberGenerator numberGenerator = new CarMoveThresholdGenerator();
-        Car car = new Car(numberGenerator);
+        Car car = new Car(name, numberGenerator);
 
         // when
         car.move();
 
         // then
-        assertThat(car.getDistance()).isEqualTo(1);
+        assertThat(car.getDistance()).isEqualTo(new Distance(1));
     }
 
     @Test
     public void Car_이동_문턱값_미만() {
         // given
+        String name = "son";
         NumberGenerator numberGenerator = new ZeroGenerator();
-        Car car = new Car(numberGenerator);
+        Car car = new Car(name, numberGenerator);
 
         // when
         car.move();
 
         // then
-        assertThat(car.getDistance()).isEqualTo(0);
+        assertThat(car.getDistance()).isEqualTo(new Distance(0));
     }
 
     @Test
-    public void toString_이동한거리를_표시() {
+    public void toString_이름과_이동한거리를_표시() {
         // given
+        String name = "son";
         NumberGenerator numberGenerator = new CarMoveThresholdGenerator();
-        Car car = new Car(numberGenerator);
+        Car car = new Car(name, numberGenerator);
 
         // when
         car.move();
         car.move();
 
         // then
-        assertThat(car.toString()).isEqualTo("--");
+        assertThat(car.toString()).isEqualTo(name + " : --");
     }
 }
