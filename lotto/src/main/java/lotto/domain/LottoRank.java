dc8bfb7b21a37e891111c516c33fd9aa3a5d66e1
devson <sonyc5720@gmail.com>
2019-04-23 13:20:29 +0900

    로또 순위를 나타내는 LottoRank 구현

---
+++ lotto/src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import lotto.vo.Money;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+    FIRST(new Money(2_000_000_000), 6),
+    SECOND(new Money(1_500_000), 5),
+    THIRD(new Money(50_000), 4),
+    FOURTH(new Money(5_000), 3),
+    FAIL(new Money(0), 0);
+
+    private final Money winningMoney;
+    private final int duplicatedNumber;
+
+    LottoRank(Money money, int duplicatedNumber) {
+        winningMoney = money;
+        this.duplicatedNumber = duplicatedNumber;
+    }
+
+    static LottoRank of(int duplicatedNumber) {
+        return Arrays.stream(LottoRank.values())
+                .filter(lottoRank -> (lottoRank.duplicatedNumber == duplicatedNumber))
+                .findFirst()
+                .orElse(LottoRank.FAIL);
+    }
+}
